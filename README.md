3fffd8a35db63c1a99bf36105c0b0d6e85ea6f06
KavyaMehta22 <71765245+KavyaMehta22@users.noreply.github.com>
2020-11-10 14:34:23 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Class-35
+# asynchronousBallMovement
+Boiler plate for ball moving aysnchronously
---
+++ sketch.js
@@ -0,0 +1,48 @@
+var ball;
+var database, position;
+
+function setup(){
+    database = firebase.database();
+    
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    var ballPosition = database.ref("ball/position");
+    ballPosition.on("value", readPosition, showError);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+   database.ref("ball/position").set({
+      "x":position.x+x, 
+     "y":position.y+y 
+   })
+}
+
+function readPosition(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function showError(){
+    console.log("error in database");
+}
